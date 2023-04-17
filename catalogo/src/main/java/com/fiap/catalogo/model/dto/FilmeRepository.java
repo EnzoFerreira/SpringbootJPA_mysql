0aa4a1e12d3cef20831eb03ed6d277673079006c
Enzo Ferreira <91428282+EnzoFerreira@users.noreply.github.com>
2023-04-17 15:50:54 -0300

    Delete catalogo directory

--- catalogo/src/main/java/com/fiap/catalogo/model/dto/FilmeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.catalogo.model.dto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fiap.catalogo.model.entity.Filme;
-
-public interface FilmeRepository extends JpaRepository<Filme, Long>{
-
-}
